154cf0b447fc93673b128e6a3575201dd44c7f4b
francimar <francimar@1fa74207-a24e-4084-9b10-fb5fcecf4b73>
2012-03-18 02:41:47 +0000

    fix correct functions

--- tests/outline and code completion/test_namespace.cpp
+++ tests/outline and code completion/test_namespace.cpp
@@ -23,17 +23,17 @@ namespace MyExternalNamespace
 
     namespace MyInternalNamespace
     {
-        void TestNamespace::private_function()
+        void TestNamespace::private_function2()
         {
 
         }
 
-        void TestNamespace::protected_function()
+        void TestNamespace::protected_function2()
         {
 
         }
 
-        void TestNamespace::public_function()
+        void TestNamespace::public_function2()
         {
 
         }
